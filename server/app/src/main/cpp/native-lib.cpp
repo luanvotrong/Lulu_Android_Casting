eb232ebdc2715f244af7244fc759ed7afc563cf6
luanvotrong <vtluan288@gmail.com>
2016-10-26 11:38:18 +0700

    - Update

--- server/app/src/main/cpp/native-lib.cpp
+++ server/app/src/main/cpp/native-lib.cpp
@@ -3,7 +3,7 @@
 
 extern "C"
 jstring
-Java_com_example_luanvotrong_server_MainActivity_stringFromJNI(
+Java_com_luanvotrong_server_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
